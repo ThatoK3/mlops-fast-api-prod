3b205bd1cdc9a1154f03ee657796f65f7ab58529
Thato Kekana <ThatoJoe@outlook.com>
2025-09-03 20:35:02 +0000

    mysql->mssql and s3 sink

--- run.sh
+++ run.sh
@@ -3,8 +3,22 @@ sleep 5s
 source .env
 docker compose --env-file .env up -d --build
 
+sleep 60s
+bash upload_db_data.sh
+
 sleep 30s
 bash dbz-register-mysql-source.sh
 
 sleep 30s
 curl -s http://localhost:8083/connectors/stroke-predictions-connector/status | jq
+
+sleep 30s
+bash mssql-sink-connect.sh
+
+sleep 30s
+bash register-s3-sink.sh
+
+sleep 30s
+curl -s http://localhost:8083/connectors/
+
+
---
+++ upload_db_data.sh
@@ -0,0 +1,1 @@
+mysql -u root -pdebezium -h 172.31.81.42 stroke_predictions < stroke_predictions.sql
